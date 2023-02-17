d3579e264f04f6051a5a9a30985e1dd3838fb650
AddiH <astrid.elmann@gmail.com>
2023-02-17 14:10:43 +0100

    making fancier function

--- utils/hist_and_save_with_ross.py
+++ utils/hist_and_save_with_ross.py
@@ -1,9 +1,17 @@
 import cv2
 import matplotlib.pyplot as plt
+import os
+import sys
+sys.path.append(os.path.join(".."))
 
+'''
+Takes an image and prints the RGB histogram of the img to screen, then it saves the histogram.
+'''
 
-
-def hist(in_path, out_path):
+def ross_hist(filename):
+    # make path
+    in_path = os.path.join("..", "data", "img", filename)
+    # load img
     image = cv2.imread(in_path)
     # split channels
     channels = cv2.split(image)
@@ -26,6 +34,9 @@ def hist(in_path, out_path):
         plt.plot(hist, color=color)
         # Set limits of x-axis
         plt.xlim([0, 256])
+    # Save path
+    hist_filename = f'histogram_of_{filename}'
+    out_path = os.path.join("..", "data", "img", hist_filename)
     # Save img
     plt.savefig(out_path) # THIS HAS TO HAPPEN BEFORE PLT.SHOW()
     # Show plot
